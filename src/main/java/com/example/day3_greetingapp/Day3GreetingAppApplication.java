30a954c24ec43bb0fdb22806d19d5b65c63dbc2b
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-09-23 23:06:37 +0530

    START: Welcome to the Greeting app development program

---
+++ src/main/java/com/example/day3_greetingapp/Day3GreetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_greetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3GreetingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3GreetingAppApplication.class, args);
+    }
+
+}
